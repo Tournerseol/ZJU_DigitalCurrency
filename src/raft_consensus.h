d5400551147b0d17273d832232dde3898d1d59b2
Tianyi <43903406+Tournerseol@users.noreply.github.com>
2020-05-30 15:56:31 +0800

    Update raft_consensus.h
    5.30第二次更新

--- src/raft_consensus.h
+++ src/raft_consensus.h
@@ -3,6 +3,11 @@
 #include "election.h"
 using namespace std;
 
+// 一些声明：
+// (1) 因为函数的具体实现还没有确定，所以传参和返回类型我都没有规
+// 定好，需要自己再加工
+// (2) 有些变量后面加了下划线的原因是，初始化节点的时候结构函数传
+// 参可能会用到一模一样的名字，加下划线是一个通用的解决方法
 #ifndef RAFT_CONSENSUS_H_
 #define RAFT_CONSENSUS_H_
 
@@ -11,7 +16,7 @@ class ServerNode
 public:
     // 枚举身份类型，FOLLOWER=0, CANDIDATE=1, LEADER=2
     // 初始时刻所有节点默认为FOLLOWER身份
-    enum identity
+    enum identity_
     {
         FOLLOWER,
         CANDIDATE,
@@ -26,19 +31,24 @@ public:
     // 当节点超时，或没有收到心跳包时，转变身份为candidate
     void TransToCandidate();
 
-    // candidate特有功能
+    // 接收candidate发来的投票请求并决定赞成还是反对
+    // 投票的结果由投票助手记录
+    void RespondRequest();
 
-    // 
+    // ------------candidate功能------------
+
+    // 向其他节点发送投票请求
     void SendVoteRequest();
 
-    // 收到大多数投票时
+    // 收到大多数投票时（是否竞选成功由投票助手告知），转变身
+    // 份为leader
     void TransToLeader();
 
-    // leader特有功能
+    // ------------leader功能------------
 
 private:
     // 记录任期号
-    int term;
+    int term_;
     // 判断节点是否超时
     bool IsTimeOut();
 };
