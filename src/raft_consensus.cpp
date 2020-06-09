8fd9ff495625a128203e194fdabf34ee862b227e
Phantom-flame <60167724+Phantom-flame@users.noreply.github.com>
2020-06-09 21:38:19 +0800

    Update raft_consensus.cpp

--- src/raft_consensus.cpp
+++ src/raft_consensus.cpp
@@ -1,8 +1,150 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <ctime>
 #include <dirent.h>
+#include <windows.h>
 #include "election.h"
 #include "raft_consensus.h"
 using namespace std;
 
+VoteAssistant vote_assistant;
+
+//static int IsElected[identify,term_];//记录每个成员在当前领导人任期号下是否投过票 
+void Checklog();//比较日志消息新旧 
+
+// 返回节点的当前身份
+int ServerNode::ReturnIdentity()
+{
+    return this->ID;
+}
+
+// ---------------FOLLOWER---------------
+
+//在class ServerNode添加变量 
+//public:
+//    int ReturnNumber();
+//    ServerNode();//默认构造函数
+//    ServerNode(const ServerNode&);//默认拷贝构造函数 
+//private:
+//    int status;
+//    int number;
+ //   int HeartBeatMsg; //为1代表收到leader信号
+//	  raft_entry Log;   //raft_entry类 存储日志信息 
+
+int ServerNode::TransToCandidate()
+{
+    if (heartbeat_msg){//时限内未收到heartbeat转变为candidate
+        this->ID=CANDIDATE;
+        heartbeat_msg=0;//刷新
+        //term_++;
+        //isElected=0;
+        return 1;
+    }
+    
+    else{
+    	return 0;
+	}
+}
+
+int ServerNode::RespondRequest(ServerNode &L, ServerNode &C)
+{
+	if(L.ReturnTerm()<C.ReturnTerm()){
+		return 1;
+		
+	}	
+	
+	else if(L.ReturnTerm() == C.ReturnTerm()){
+		if(isElected == 0 && Checklog()==1){
+			election_timeout_=rand()%200+100;
+			isElected=1;
+			return 1;
+		}
+		else
+			return 0; 
+	}
+	else{
+		return 0;
+	}
+} 
+
+int ServerNode::ReceiveAppendEntries(ServerNode &L)
+{
+    if(L.ReturnIdentity()==LEADER && heartbeat_msg){
+    	//确认leader已经发送心跳包 
+    	
+        election_timeout_=rand()%200+100;//更新election_timeout_
+        
+        return 1;
+        
+    }
+    else{
+    	
+        return 0;
+        
+    }
+    
+}
+
+void ServerNode::ReplicateLog(ServerNode &L)
+{
+    if(heartbeat_msg && L.ReturnIdentity()==LEADER){
+        //写入日志
+        //Log.data = L.Log.data
+    }
+}
+
+void ServerNode::ResetMsg() {
+	//身份为follower且heartbeat_msg=1的节点需调用该函数刷新 
+    while(this->ReturnIdentity()==FOLLOWER)
+    {
+        Sleep(election_timeout_+50);
+        heartbeat_msg=0;
+    }
+}
+
+// ---------------CANDIDATE---------------
+void ServerNode::SendVoteRequest()
+{
+
+}
+
+void ServerNode::TransToLeader()
+{
+    // 如果选举结果所返回的新leader的下标等于自身的下标，则变为leader
+	if (vote_assistant.ElectionResult() == this->self_number) {
+        this->ID = LEADER;
+        // 更新自己的心跳超时时间
+	    this->heartbeat_timeout_ = rand()%200;
+	}
+}
+
+// ---------------leader功能---------------
+
+// 接收客户端发来的交易信息变动，并写入leader的日志
+void ServerNode::ReceiveClientChange(string send, string receive, double amount)
+{
+	Log::Write(send + " " + receive + " " + to_string(amount));
+}
+
+// 向follower发送心跳包
+void ServerNode::SendAppendEntries()
+{
+	while (1){
+		Sleep(heartbeat_timeout_;);
+		for (int i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
+			if (s[i].ID == FOLLOWER) {
+				s[i].heartbeat_msg = 1;
+			}
+		}
+	}
+}
+
+// 当大多数节点收到变动信息后，leader反馈给客户端
+bool ServerNode::CommitEntry(int entry)
+{
+	int n = sizeof(s) / sizeof(s[0]);
+	int majority_size = n % 2 ? n / 2 + 1: n / 2;
+	if (entry >= majority_size)	return true;
+	else  return false;
+}
