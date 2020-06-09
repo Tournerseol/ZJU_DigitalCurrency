a03855cecde8de25e00a873dce13103c062c092f
Tianyi <43903406+Tournerseol@users.noreply.github.com>
2020-06-09 17:07:39 +0800

    Update raft_consensus.cpp

--- src/raft_consensus.cpp
+++ src/raft_consensus.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <ctime>
 #include <dirent.h>
+#include <windows.h>
 #include "election.h"
 #include "raft_consensus.h"
 using namespace std;
@@ -116,25 +117,32 @@ void ServerNode::TransToLeader()
 	}
 }
 
-// ---------------LEADER---------------
-void ServerNode::ReceiveClientChange(string send, string receive, double amount)
+// ---------------leader功能---------------
+
+// 接收客户端发来的交易信息变动，并写入leader的日志
+void ReceiveClientChange(string send, string receive, double amount)
 {
-    Log::Write(send + " " + receive + " " + to_string(amount));
+	Log::Write(send + " " + receive + " " + to_string(amount));
 }
 
-void ServerNode::SendAppendEntries()
+// 向follower发送心跳包
+void SendAppendEntries()
 {
-    for (int i = 0; i < sizeof(nodes) / sizeof(nodes[0]); i++) {
-        if (nodes[i].identity == FOLLOWER) {
-            //向nodes[i]发送心跳包
-        }
-    }
+	while (1){
+		Sleep(HeartbeatTimeout);
+		for (i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
+			if (s[i].identity == FOLLOWER) {
+				s[i].HeartbeatMsg = 1;
+			}
+		}
+	}
 }
 
-bool ServerNode::CommitEntry(int entry)
+// 当大多数节点收到变动信息后，leader反馈给客户端
+bool CommitEntry(int entry)
 {
-    int n = sizeof(nodes) / sizeof(nodes[0]);
-    int majority_size = n % 2 ? n / 2 + 1: n / 2;
-    if (entry >= majority_size)	return true;
-    else  return false;
+	int n = sizeof(s) / sizeof(s[0]);
+	int majority_size = n % 2 ? n / 2 + 1: n / 2;
+	if (entry >= majority_size)	return true;
+	else  return false;
 }
