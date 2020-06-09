e7ddb2fe594065998420f6f0c8fd5d206d6b458e
Tianyi <43903406+Tournerseol@users.noreply.github.com>
2020-06-09 17:18:09 +0800

    Update raft_consensus.cpp

--- src/raft_consensus.cpp
+++ src/raft_consensus.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include <ctime>
 #include <dirent.h>
-#include <windows.h>
+// #include <windows.h>
 #include "election.h"
 #include "raft_consensus.h"
 using namespace std;
@@ -120,26 +120,26 @@ void ServerNode::TransToLeader()
 // ---------------leader功能---------------
 
 // 接收客户端发来的交易信息变动，并写入leader的日志
-void ReceiveClientChange(string send, string receive, double amount)
+void ServerNode::ReceiveClientChange(string send, string receive, double amount)
 {
 	Log::Write(send + " " + receive + " " + to_string(amount));
 }
 
 // 向follower发送心跳包
-void SendAppendEntries()
+void ServerNode::SendAppendEntries()
 {
 	while (1){
 		Sleep(HeartbeatTimeout);
-		for (i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
-			if (s[i].identity == FOLLOWER) {
-				s[i].HeartbeatMsg = 1;
+		for (int i = 0; i < sizeof(s) / sizeof(s[0]); i++) {
+			if (s[i].ID == FOLLOWER) {
+				s[i].heartbeat_msg = 1;
 			}
 		}
 	}
 }
 
 // 当大多数节点收到变动信息后，leader反馈给客户端
-bool CommitEntry(int entry)
+bool ServerNode::CommitEntry(int entry)
 {
 	int n = sizeof(s) / sizeof(s[0]);
 	int majority_size = n % 2 ? n / 2 + 1: n / 2;
